d87e502777481a6c58fa02930e891de688989b47
Alexey Sevastyanov <sevastianov1@yandex.ru>
2020-02-21 21:05:47 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let someStr = 'Изменил, сохранил, собрал';
+//console.log('Hello, Webpack!')
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
+import "./style.css";
